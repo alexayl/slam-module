161d5bab968489139ea7cb1aad5a18bbd6b812ec
Alex Aylward <alexaylward@outlook.com>
2025-07-09 18:34:53 -0500

    Add lidar polling

--- app/include/hardware_intf.h
+++ app/include/hardware_intf.h
@@ -1,6 +1,8 @@
 #ifndef HARDWARE_INTF_H
 #define HARDWARE_INTF_H
 
+void get_imu_device(void);
+void get_lidar_device(void);
 void app_hardware_init(void);
 
 #endif // HARDWARE_INTF_H
--- app/src/lidar.c
+++ app/src/lidar.c
@@ -1,6 +1,40 @@
 #include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/sensor.h>
+#include <stdio.h>
+
+#include "hardware_intf.h"
+#include "lidar.h"
+
+#define DEBUG_LIDAR
 
 void lidar_thread(void *arg1, void *arg2, void *arg3) {
+
+    const struct device *dev = get_lidar_device();
+    struct sensor_value distance;
+
+    int rc;
+
+    while (1) {
+        rc = sensor_sample_fetch(dev);
+
+        if (rc == 0) {
+            rc = sensor_channel_get(dev, SENSOR_CHAN_DISTANCE,
+                        &distance);
+        }
+        if (rc == 0) {
+            #ifdef DEBUG_LIDAR
+            printk("Distance: %g m\n", sensor_value_to_double(&distance));
+            #endif
+        } else {
+            #ifdef DEBUG_LIDAR
+            printk("sample fetch/get failed: %d\n", rc);
+            #endif
+        }
+
+        k_sleep(K_MSEC(100)); // Sleep for 100 milliseconds
+
+    }
 
     return;
 }
